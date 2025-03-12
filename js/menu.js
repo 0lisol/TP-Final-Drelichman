4a5ea5046bafac7c51dc99731537d456edda3bec
0lisol <iaravelazquezalvarez@gmail.com>
2025-03-12 00:01:08 -0300

    sigo arreglando el Nav de a poco, elegiendo rutas y definiendolas,agregue correctamente el java script de menu, arregle el menu de hamburguesa un poco mas arreglado y hice otro archivo css. PD: A SEGUIIIIIR

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,15 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
+document.addEventListener("DOMContentLoaded", function() { //* declaramos la funcion que va a actuar una vez que cargue el DOM (documento)*//
 
-    menuToggle.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
+    let botonmenu = document.querySelector(".menu-toggle"); //* aca seleccionamos el boton de la hamburguesa, lo capturamos.
+    let listademenu = document.querySelector(".nav-links");//* seleccionamos el nav link
+
+    botonmenu.addEventListener("click",  function() { //* aca le decmos al boton , primero que lo escuchamos y luego que cuando haga click pase esto:
+        listademenu.classList.toggle("active"); //* 
     });
 });
+
+
+
+
+
